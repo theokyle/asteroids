c4a59eab08253cdbfd560d3b7396bb2556f73e42
theokyle <theokyle@gmail.com>
2025-05-18 00:13:16 -0500

    add clock object for fps setting

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from constants import *
 def main():
     print("Starting Asteroids!")
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -12,6 +16,7 @@ def main():
                 return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
